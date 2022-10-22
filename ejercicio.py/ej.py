a742db244826f498add07ba3f97e3fb5f42e8c24
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-10-22 18:05:01 +0200

    ejercicio hecho
    comit final de ejercicio.

--- ejercicio.py/ej.py
+++ ejercicio.py/ej.py
@@ -17,25 +17,68 @@ class Coche(Vehiculo):
         return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
 
 
-class Bicicleta(Vehiculo):
-    def __init__(self, color, ruedas, tipo):
-        Vehiculo.__init__(self, color, ruedas)
-        self.tipo = tipo
-
+class Camioneta(Coche):
+    
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
+        self.carga = carga
+        
     def __str__(self):
-        return Vehiculo.__str__(self) + ", {} es su tipo".format(self.tipo)
+        return super().__str__() + ", {} kg de carga".format(self.carga)
+    
 
+class Bicicleta(Vehiculo):
+    
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+        
+    def __str__(self):
+        return super().__str__() + ", {}".format(self.tipo)
+    
 class Motocicleta(Bicicleta):
+    
     def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
         super().__init__(color, ruedas, tipo)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
+        
     def __str__(self):
-        return Bicicleta.__str__(self) + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)    
+        return super().__str__() + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada) 
+    
+    
+def catalogar(vehiculos):
+    for v in vehiculos:
+        print(type(v).__name__, v)
+        
+def catalogar(vehiculos, ruedas=None):
+      
+    # Primera pasada, mostrar recuento
+    if ruedas != None:
+        contador = 0
+        for v in vehiculos:
+            if v.ruedas == ruedas: 
+                contador += 1
+        print("Se han encontrado {} vehículos con {} ruedas:".format(contador, ruedas))
 
-class Camioneta(Coche):
-    def __init__(self, color, ruedas, velocidad, cilindrada,carga):
-        super().__init__(color, ruedas, velocidad, cilindrada)
-        self.carga = carga
-    def __str__(self):
-        return Coche().__str__(self) + ", {} kg".format(self.carga)
+    
+    # Segnda pasada, mostrar vehículos
+    for v in vehiculos:
+        if ruedas == None:
+            print(type(v).__name__, v)
+        else:
+            if v.ruedas == ruedas:
+                print(type(v).__name__, v)
+                
+        
+lista = [
+    Coche("rojo", 4, 300, 3200),
+    Camioneta("blanco", 4, 120, 1300, 2500),
+    Bicicleta("azul", 2, "deportiva"),
+    Motocicleta("naranja", 2, "urbana", 280, 1000)
+]
+
+catalogar(lista)
+catalogar(lista, 0)
+catalogar(lista, 2)
+catalogar(lista, 4)        
